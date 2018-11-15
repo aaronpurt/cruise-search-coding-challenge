e69f20b5f9a50cfbf80ee32543601b567bb2d448
Rattanak Chea <rattanak.chea@gmail.com>
2018-11-14 16:10:05 -0800

    add material theme, default routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ItinerariesComponent } from "./itineraries/itineraries.component";
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, ItinerariesComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
