f803cbaae8638e143e25aeafda11a058909a4dee
Rattanak Chea <rattanak.chea@gmail.com>
2018-11-14 16:19:48 -0800

    added router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { BookingComponent } from "./booking/booking.component";
+import { ItinerariesComponent } from "./itineraries/itineraries.component";
+const routes: Routes = [
+  { path: "", component: ItinerariesComponent },
+  { path: "booking", component: BookingComponent }
+];
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ItinerariesComponent } from "./itineraries/itineraries.component";
 import { AppRoutingModule } from './/app-routing.module';
+import { BookingComponent } from './booking/booking.component';
 
 @NgModule({
-  declarations: [AppComponent, ItinerariesComponent],
+  declarations: [AppComponent, ItinerariesComponent, BookingComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
